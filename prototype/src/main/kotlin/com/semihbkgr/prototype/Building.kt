08673db74c5115b252cf0de8fc2b6d9e1283c6b7
SemihBG <buyukgungorsemih@gmail.com>
2021-09-20 22:33:35 +0300

    Create prototype completion of README.md and source code review

--- prototype/src/main/kotlin/com/semihbkgr/prototype/Building.kt
+++ prototype/src/main/kotlin/com/semihbkgr/prototype/Building.kt
@@ -1,12 +1,12 @@
 package com.semihbkgr.prototype
 
-abstract class Building (var x:Int,var y:Int,var color:String) {
+abstract class Building(var x: Int, var y: Int, var color: String) {
 
-    constructor(building:Building):this(building.x,building.y,building.color)
+    constructor(building: Building) : this(building.x, building.y, building.color)
 
-    abstract fun clone():Building
+    abstract fun clone(): Building
 
-    override fun toString(): String ="x: $x, y: $y, color: $color"
+    override fun toString(): String = "x: $x, y: $y, color: $color"
 
 }
 
--- prototype/src/main/kotlin/com/semihbkgr/prototype/Castle.kt
+++ prototype/src/main/kotlin/com/semihbkgr/prototype/Castle.kt
@@ -1,11 +1,11 @@
 package com.semihbkgr.prototype
 
-class Castle(x:Int,y:Int,color:String,var height:Float):Building(x,y,color) {
+class Castle(x: Int, y: Int, color: String, var height: Float) : Building(x, y, color) {
 
-    constructor(tower:Castle):this(tower.x,tower.y,tower.color,tower.height)
+    constructor(tower: Castle) : this(tower.x, tower.y, tower.color, tower.height)
 
-    override fun clone(): Castle= Castle(this)
+    override fun clone(): Castle = Castle(this)
 
-    override fun toString(): String = super.toString()+" height: $height"
+    override fun toString(): String = "Castle: " + super.toString() + " height: $height"
 
 }
